71594b043676b7a649f84838be2ee938b4ff7ffb
bbugs <ndo.hoffman@gmail.com>
2016-07-24 12:05:08 +0200

    synced matlab check_python_cost.m and test_multimodal_net_v2.py to test loss and gradients against matlab code

--- cs231n/multimodal/test_multimodal_net_v2.py
+++ cs231n/multimodal/test_multimodal_net_v2.py
@@ -3,7 +3,7 @@ Test the MultiModalNet against matlab implementation
 susy_code_bare_bones/check_python_cost.m
 """
 
-
+import os
 from cs231n.multimodal import multimodal_net
 import numpy as np
 import math
@@ -12,29 +12,53 @@ import scipy.io as sio
 from cs231n.tests import test_utils
 
 rpath = '../../nips2014_karpathy/susy_code_bare_bones/'
+
+####################################################################
+# Set loss parameters
+####################################################################
+loss_params = {}
+
+loss_params['reg'] = 0.
+loss_params['finetuneCNN'] = False
+
+# local loss params
+loss_params['uselocal'] = uselocal = True
+loss_params['local_margin'] = local_margin = 1.
+loss_params['local_scale'] = local_scale = 1.
+loss_params['do_mil'] = do_mil = True
+
+# global loss params
+loss_params['useglobal'] = useglobal = False
+loss_params['global_margin'] = global_margin = 40.
+loss_params['global_scale'] = global_scale = 1.
+loss_params['smooth_num'] = smotth_num = 5.
+loss_params['global_method'] = global_method = 'maxaccum'
+loss_params['thrglobalscore'] = thrglobalscore = False
+
+sio.savemat(rpath + 'loss_params.mat', {'loss_params': loss_params})
+
+####################################################################
+# Create random data
+####################################################################
 np.random.seed(42)
 
-N = 3  # number of image-sentence pairs in batch
+N = 23  # number of image-sentence pairs in batch
 
 n_sent_per_img = 5
 n_sent = n_sent_per_img * N
 
-n_region_per_img = 4
+n_region_per_img = 3
 n_regions = n_region_per_img * N
 
-n_words_per_img = 7
+n_words_per_img = 4
 n_words = n_words_per_img * N
 
 region2pair_id = test_utils.mk_random_pair_id(n_region_per_img, N)
 word2pair_id = test_utils.mk_random_pair_id(n_words_per_img, N)
 
-
-print region2pair_id
-print word2pair_id
-
-img_input_dim = 16 # size of cnn
-txt_input_dim = 8  # size of word2vec pretrained vectors
-hidden_dim = 4  # size of multimodal space
+img_input_dim = 13 # size of cnn
+txt_input_dim = 11  # size of word2vec pretrained vectors
+hidden_dim = 7  # size of multimodal space
 
 std_img = math.sqrt(2. / img_input_dim)
 std_txt = math.sqrt(2. / txt_input_dim)
@@ -43,6 +67,9 @@ weight_scale = {'img': std_img, 'txt': std_txt}
 X_img = np.random.randn(n_regions, img_input_dim)
 X_txt = np.random.randn(n_words, txt_input_dim)
 
+####################################################################
+# Initialize multimodal net
+####################################################################
 
 mmnet = multimodal_net.MultiModalNet(img_input_dim, txt_input_dim, hidden_dim, weight_scale, seed=42)
 
@@ -61,25 +88,35 @@ sio.savemat(rpath + 'region2pair_id.mat', {'region2pair_id': region2pair_id})
 sio.savemat(rpath + 'word2pair_id.mat', {'word2pair_id': word2pair_id})
 
 
-mmnet.set_global_score_hyperparams(global_margin=40., global_scale=1.,
-                                   smooth_num=5., global_method='maxaccum',
-                                   thrglobalscore=False)
+mmnet.set_global_score_hyperparams(global_margin=global_margin, global_scale=global_scale,
+                                   smooth_num=smotth_num, global_method=global_method,
+                                   thrglobalscore=thrglobalscore)
 
-mmnet.set_local_hyperparams(local_margin=1., local_scale=1., do_mil=True)
+mmnet.set_local_hyperparams(local_margin=local_margin, local_scale=local_scale, do_mil=do_mil)
 
-loss, grads = mmnet.loss(X_img, X_txt, region2pair_id, word2pair_id, useglobal=True, uselocal=True)
+loss, grads = mmnet.loss(X_img, X_txt, region2pair_id, word2pair_id,
+                         useglobal=useglobal, uselocal=uselocal)
 
-print loss
-print grads
+print "loss", loss
+# print grads
+
+###################
+# Call Matlab check_python_cost.m to create the
 
 # TODO: in the matlab code, save the cost and gradients, then load them here and compare resutls.
 # For now, I just used a visual inspection and everything checks, except for the gradient wrt Wsem
 # because I cannot compute it in matlab because I don't have depTrees and have to spend more time
 # re-writing BackwardSents to not use depTrees.
 
+os.system("matlab -nojvm -nodesktop < {0}/check_python_cost.m".format(rpath))
 
+matlab_output = sio.loadmat(rpath + 'matlab_output.mat')
 
+print matlab_output['cost']
+print matlab_output['df_Wi2s']
 
+assert np.allclose(loss, matlab_output['cost'][0][0])
+assert np.allclose(grads['Wi2s'].T, matlab_output['df_Wi2s'])
 
 # y = np.random.randint(N_words, size=N_img)  # indicate which word is correct (happens together) with each image
 
