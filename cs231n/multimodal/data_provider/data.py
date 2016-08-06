afe27ab0b184d1c4cb448fb1d262992b0fbc0633
bbugs <ndo.hoffman@gmail.com>
2016-08-06 16:58:47 +0200

    on data.py, added AlginenmentData, JsonData, JsonFile, CnnData and extended Word2VecData

--- cs231n/multimodal/data_provider/data.py
+++ cs231n/multimodal/data_provider/data.py
@@ -1,6 +1,255 @@
 import numpy as np
 import csv
 import time
+import json
+
+
+def gen_word_to_pair_id(word_seq):
+    """(list of np arrays) -> np array
+
+    """
+
+    N = len(word_seq)
+
+    word2pair_id = np.array([])  # empty array
+
+    for i in range(N):
+        word_ids = word_seq[i]
+        pair_ids = i * np.ones(word_ids.shape, dtype=int)
+
+        word2pair_id = np.hstack((word2pair_id, pair_ids))
+
+    return word2pair_id
+
+
+class AlignmentData(object):
+
+    def __init__(self, data_config, split):
+        self.d = data_config
+        self.split = split
+        if split == 'train':
+            fname = self.d['json_path_train']
+        elif split == 'val':
+            fname = self.d['json_path_val']
+        elif split == 'test':
+            fname = self.d['json_path_test']
+        else:
+            raise ValueError("only treain, val and test splits supported")
+        self.json_file = JsonFile(fname)
+
+        return
+
+    @staticmethod
+    def make_region2pair_id(img_ids, num_regions_per_img):
+        region2pair_id = np.zeros((len(img_ids) * num_regions_per_img, ))
+
+        i = 0
+        for img_id in img_ids:
+            region2pair_id[i: i + num_regions_per_img] = i
+            i += num_regions_per_img
+
+        return region2pair_id
+
+    def make_word2pair_id(self, img_ids):
+        word2pair_id = np.array([])  # empty array
+
+        counter = 0
+        for img_id in img_ids:
+            unique_word_list = self.json_file.get_word_list_of_img_id(img_id)
+
+            n_words = len(unique_word_list)
+            pair_ids = counter * np.ones(n_words, dtype=int)
+
+            word2pair_id = np.hstack((word2pair_id, pair_ids))
+            counter += 1
+
+        return word2pair_id
+
+    @staticmethod
+    def pair_id2y(region2pair_id, word2pair_id):
+
+        N = np.max(region2pair_id)
+        assert N == np.max(word2pair_id)
+
+        n_regions = region2pair_id.shape[0]
+        n_words = word2pair_id.shape[0]
+        y = -np.ones((n_regions, n_words))
+
+        for i in range(N + 1):
+            MEQ = np.outer(region2pair_id == i, word2pair_id == i)
+            y[MEQ] = 1
+
+        return y
+
+    def make_y_true_txt2img(self):
+        """
+        y is y_true_zappos_img for txt2img
+        """
+
+        return
+
+    def make_y_true_img2txt(self):
+        """
+        y is y_true_all_vocab for img2txt
+        """
+
+
+        return
+
+
+class JsonFile(object):
+
+    def __init__(self, json_fname):
+        with open(json_fname, 'r') as f:
+            self.dataset = json.load(f)
+
+        return
+
+    def get_ids_split(self, target_split='train'):
+        ids = []
+        for item in self.dataset['items']:
+            imgid = item['imgid']
+            split = item['split']
+            if split == target_split:
+                ids.append(imgid)
+        return ids
+
+    def get_item_from_img_id(self, target_img_id):
+        item = None
+        for item in self.dataset['items']:
+            imgid = item['imgid']
+            if imgid == target_img_id:
+                break  # found teh right item
+        return item
+
+    def get_word_list_of_img_id(self, img_id):
+        """
+        return a list of unique words that correspond to the img_id
+        """
+        word_list = []
+        item = self.get_item_from_img_id(img_id)
+        if item is None:
+            return word_list
+        txt = item['text']
+        word_list = list(set([w.replace('\n') for w in txt.split(" ")]))
+        return word_list
+
+
+    def get_text_of_img_ids(self, img_ids):
+        id2text = {}
+        for img_id in img_ids:
+            # get item from json
+            item = self.get_item_from_img_id(img_id)
+            if item is None:
+                print img_id, " not found"
+                continue
+            txt = item['text']
+            id2text[img_id] = txt
+        return id2text
+
+
+class JsonData(object):
+
+    def __init__(self, data_config):
+        self.d = data_config
+
+        self.json_train = {}
+        self.json_val = {}
+        self.json_test = {}
+
+        return
+
+    def set_json_split(self, split='test'):
+        if split == 'train':
+            fname = self.d['cnn_regions_path_train']
+            self.json_train = np.loadtxt(fname, delimiter=',')
+        elif split == 'val':
+            fname = self.d['cnn_regions_path_val']
+            self.json_val = np.loadtxt(fname, delimiter=',')
+        elif split == 'test':
+            fname = self.d['cnn_regions_path_test']
+            self.json_test = np.loadtxt(fname, delimiter=',')
+        else:
+            raise ValueError("only train, val and test splits supported")
+        return
+
+
+class CnnData(object):
+
+    def __init__(self, data_config):
+        self.d = data_config
+
+        # full image cnn
+        self.cnn_full_img_train = np.array([])
+        self.cnn_full_img_val = np.array([])
+        self.cnn_full_img_test = np.array([])
+
+        # full image + regions cnn
+        self.cnn_region_train = np.array([])
+        self.cnn_region_val = np.array([])
+        self.cnn_region_test = np.array([])
+
+        return
+
+    def set_cnn_full_img_split(self, split='test'):
+        if split == 'train':
+            fname = self.d['cnn_full_img_path_train']
+            self.cnn_full_img_train = np.loadtxt(fname, delimiter=',')
+        elif split == 'val':
+            fname = self.d['cnn_full_img_path_val']
+            self.cnn_full_img_val = np.loadtxt(fname, delimiter=',')
+        elif split == 'test':
+            fname = self.d['cnn_full_img_path_test']
+            self.cnn_full_img_test = np.loadtxt(fname, delimiter=',')
+        else:
+            raise ValueError("only train, val and test splits supported")
+        return
+
+    def set_cnn_regions_split(self, split):
+        if split == 'train':
+            fname = self.d['cnn_regions_path_train']
+            self.cnn_region_train = np.loadtxt(fname, delimiter=',')
+        elif split == 'val':
+            fname = self.d['cnn_regions_path_val']
+            self.cnn_region_val = np.loadtxt(fname, delimiter=',')
+        elif split == 'test':
+            fname = self.d['cnn_regions_path_test']
+            self.cnn_region_test = np.loadtxt(fname, delimiter=',')
+        else:
+            raise ValueError("only train, val and test splits supported")
+        return
+
+    def get_cnn_regions_split(self, split):
+        if split == 'train':
+            if self.cnn_region_train.size == 0:
+                self.set_cnn_regions_split(split)
+            return self.cnn_region_train
+        elif split == 'val':
+            if self.cnn_region_val.size == 0:
+                self.set_cnn_regions_split(split)
+            return self.cnn_region_val
+        elif split == 'test':
+            if self.cnn_region_test.size == 0:
+                self.set_cnn_regions_split(split)
+            return self.cnn_region_test
+        else:
+            raise ValueError("only train, val and test splits supported")
+
+    def get_cnn_full_img_split(self, split):
+        if split == 'train':
+            if self.cnn_full_img_train.size == 0:
+                self.set_cnn_full_img_split(split)
+            return self.cnn_full_img_train
+        elif split == 'val':
+            if self.cnn_full_img_val.size == 0:
+                self.set_cnn_full_img_split(split)
+            return self.cnn_full_img_val
+        elif split == 'test':
+            if self.cnn_full_img_test.size == 0:
+                self.set_cnn_full_img_split(split)
+            return self.cnn_full_img_test
+        else:
+            raise ValueError("only train, val and test splits supported")
 
 
 class Word2VecData(object):
@@ -28,6 +277,7 @@ class Word2VecData(object):
         return X_img, X_txt, region2pair_id, word2pair_id
 
     def set_word_vectors(self, verbose=False):
+        # TODO: consider using linecache to load only the lines that you need and not the entire word2vec matrix
         # load word2vec_vocab if not already loaded
         if len(self.word2vec_vocab) == 0:
             if verbose:
@@ -70,7 +320,7 @@ class Word2VecData(object):
         i = 0
         for word in self.external_vocab:
             if word not in word2id:
-                # TODO: chnge ngrams from _ to -.
+                # TODO: change ngrams from _ to -.
                 # TODO: initialize weights randomly if not found in word2vec
                 print word + " not in word2vec"
                 i += 1
@@ -81,11 +331,19 @@ class Word2VecData(object):
         return
 
     def get_external_word_vectors(self):
+        """
+        Convinience method to return X_txt_zappos
+        """
         if self.external_word_vectors.size == 0:
             self.set_external_word_vectors()
         return self.external_word_vectors
 
     def get_word_vectors(self, external_vocab=False):
+        """
+        Convinience method to return either
+        X_txt_word2vec (external_vocab=False) or
+        X_txt_zappos (external_vocab=True)
+        """
 
         # return the subset of word vectors for the external vocabulary
         if external_vocab:
@@ -101,6 +359,7 @@ class Word2VecData(object):
 
     def get_word_vectors_of_word_list(self, word_list):
         if self.word2vec_dim == 0:
+            print "loading word vectors ..."
             self.set_word_vectors()
 
         word2id, id2word = self.get_vocab_dicts(vocab_name='word2vec')
@@ -109,8 +368,15 @@ class Word2VecData(object):
 
         i = 0
         for word in word_list:
+            if word not in word2id:
+                # TODO: initiliaze weights randomly if word not found in word2vec (or set it to a common word?)
+                print word, " not in word2vec"
+                i += 1
+                continue
+
             id = word2id[word]
             X_txt[i, :] = self.word2vec_vectors[id, :]
+            i += 1
 
         return X_txt
 
--- cs231n/multimodal/data_provider/data_unitest.py
+++ cs231n/multimodal/data_provider/data_unitest.py
@@ -7,11 +7,19 @@ d['root_path'] = root_path = '../../../../DeepFashion/'  # assume module is run 
 
 d['num_regions_per_img'] = 4 + 1
 
-# Image CNN features
+# Image CNN (Full Image + Regions) features
+d['num_regions_per_img'] = 4 + 1
 d['cnn_regions_path'] = root_path + 'data/fashion53k/img_regions/4_regions_cnn/per_split/'
 d['cnn_regions_path_train'] = d['cnn_regions_path'] + '/cnn_fc7_train.txt'
 d['cnn_regions_path_val'] = d['cnn_regions_path'] + '/cnn_fc7_val.txt'
 d['cnn_regions_path_test'] = d['cnn_regions_path'] + '/cnn_fc7_test.txt'
+
+# Image CNN (Full Image only) features
+# TODO: Get cnn features for full images only
+d['cnn_full_img_path'] = root_path + '/data/fashion53k/full_img/per_split/'
+d['cnn_full_img_path_train'] = d['cnn_full_img_path'] + '/cnn_fc7_train.txt'
+d['cnn_full_img_path_val'] = d['cnn_full_img_path'] + '/cnn_fc7_val.txt'
+d['cnn_full_img_path_test'] = d['cnn_full_img_path'] + '/cnn_fc7_test.txt'
 
 # Text features
 d['json_path'] = root_path + 'data/fashion53k/json/with_ngrams/'
@@ -22,22 +30,24 @@ d['json_path_test'] = d['json_path'] + 'dataset_dress_all_test.clean.json'
 # Word2vec vectors and vocab
 d['word2vec_vocab'] = root_path + 'data/word_vects/glove/vocab.txt'
 d['word2vec_vectors'] = root_path + 'data/word_vects/glove/vocab_vecs.txt'
-d['word2vec_dim'] = 200
 
 # External vocabulary
 d['external_vocab'] = root_path + 'data/fashion53k/external_vocab/zappos.vocab.txt'
 
 
 dd = data.Word2VecData(d)
-dd.set_word2vec_vocab()
+# dd.set_word2vec_vocab()
 # print dd.get_vocab(vocab_name='word2vec')
 
 # dd.set_word_vectors(verbose=True)
 
-dd.set_external_vocab()
+# dd.set_external_vocab()
 # X_txt_zappos = dd.set_external_word_vectors()
 
-X_txt_zappos = dd.get_word_vectors(external_vocab=True)
+# X_txt_zappos = dd.get_word_vectors(external_vocab=True)
+
+
+print dd.get_word_vectors_of_word_list(['random_stuff', 'cat', 'is', 'nice', 'cat'])
 
 print ""
 # print dd.get_vocab_dicts(vocab_name='external')
