b2d361581c951e092dd8d5ffc32a9f649d8737db
bbugs <ndo.hoffman@gmail.com>
2016-08-16 12:05:15 +0200

    added test to word2vec_data_unitest.py

--- cs231n/multimodal/data_provider/data_tests/word2vec_data_unitest.py
+++ cs231n/multimodal/data_provider/data_tests/word2vec_data_unitest.py
@@ -1,5 +1,6 @@
 from cs231n.multimodal.data_provider import word2vec_data
 from cs231n.multimodal.data_provider.data_tests import data_config
+import numpy as np
 
 d = data_config.dc
 
@@ -17,5 +18,16 @@ dd = word2vec_data.Word2VecData(w2v_vocab_fname=d['word2vec_vocab'], w2v_vectors
 
 X_txt = dd.get_word_vectors_of_word_list(['random_stuff', 'cat', 'is', 'nice', 'cat'])
 
-print X_txt
+# print X_txt
 # print X_txt.shape
+
+X_txt = dd.get_word_vectors_of_word_list(['v-neck', 'a-line'])  # 264906, 307259
+
+correct_vneck = np.array([-0.11086, -1.006, 0.11159, 0.23023, 0.29037, 0.33901, 0.62025, 0.23046, 0.043336])
+correct_aline = np.array([-0.019347, -0.60523, 0.078378, 0.20013, 0.20616, -0.12931, 0.30628, 0.025562, 0.024776])
+
+print X_txt[0, 0:9]
+print X_txt[1, 0:9]
+
+assert np.allclose(X_txt[0, 0:9], correct_vneck)
+assert np.allclose(X_txt[1, 0:9], correct_aline)
